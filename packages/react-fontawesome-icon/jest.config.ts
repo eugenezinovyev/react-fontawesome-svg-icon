a5c2fc064bf24e9eec32cd891cdb5c05f33ca92e
Eugene Zinovyev <eugene.zinovyev@gmail.com>
2021-11-14 02:43:50 +0300

    Text output for test coverage

--- packages/react-fontawesome-icon/jest.config.ts
+++ packages/react-fontawesome-icon/jest.config.ts
@@ -1,15 +1,16 @@
 export default {
-  clearMocks: true,
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  coverageReporters: [
-    "json",
-    "lcovonly",
-    "@lcov-viewer/istanbul-report"
-  ],
-  testEnvironment: "jsdom",
-  transform: {
-      "^.+\\.tsx?$": "ts-jest"
-  },
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    coverageReporters: [
+        'text',
+        'json',
+        'lcovonly',
+        '@lcov-viewer/istanbul-report',
+    ],
+    testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
 };
